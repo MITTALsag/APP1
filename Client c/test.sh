8ea6b09f82437845499b0a3e9d3d8294796eb092
MITTALsag <sagar.mittal@icloud.com>
2023-10-09 19:14:51 +0200

    version final

--- Client c/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in client-TUTORIEL2 client-projetX client-crypteMove client-BayOfPigs client-crypteSeq client-planB client-Northwoods client-LostCause 
-do 
-    ./$i
-    echo $?
-    sleep 1
-done
